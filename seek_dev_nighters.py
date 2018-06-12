61a3ffe24dc41f3ed8bd04d5f5d9ae90eb8daea3
nanobot2000 <marginal3452@yandex.ru>
2018-06-12 13:01:52 +0300

    added function load_attempts and get_midnighters

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,31 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import pytz
+import requests
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def load_attempts():
+    api_url = 'http://devman.org/api/challenges/solution_attempts/'
+    pages = requests.get(api_url).json().get('number_of_pages')
+    for page in range(1, pages+1):
+        yield from requests.get(api_url,
+                                params={'page': page}).json().get('records')
+
+
+def get_midnighters(attempts):
+    midnighters = set()
+    for attempt in attempts:
+        time_zone = pytz.timezone(attempt['timezone'])
+        attempt_time = datetime.fromtimestamp(attempt['timestamp'], time_zone)
+        if 0 <= attempt_time.hour < 6:
+            midnighters.add(attempt['username'])
+    return midnighters
+
 
 if __name__ == '__main__':
-  pass
+    attemps = load_attempts()
+    midnighters = get_midnighters(attemps)
+    print('Midnighters on Devman:')
+    print('-----------------------')
+    for index, midnighter in enumerate(midnighters, 1):
+        print(index, midnighter)
+    print('_______________________')
